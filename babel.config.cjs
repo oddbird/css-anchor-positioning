a3269d02eb1ea1a5abe0e84089d146097fc0f311
Olu Niyi-Awosusi <hello@olu.online>
2022-07-21 16:17:04 +0100

    compile to es6, put in lib

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-  ],
+    ["@babel/preset-env", { targets: { node: "6.10"  } }],
+  ]
 };
