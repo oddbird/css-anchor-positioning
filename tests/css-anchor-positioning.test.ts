d9d7e866b5bee3a3f21eb9bff442a3998962116d
Olu Niyi-Awosusi <hello@olu.online>
2022-07-27 18:13:26 +0100

    add babel-esbuild bridge

---
+++ tests/css-anchor-positioning.test.ts
@@ -0,0 +1,45 @@
+import {
+  parseCSS,
+  parseAnchorFunctions,
+  parsePositionFallback,
+} from "../src/css-anchor-positioning";
+import { sampleAnchorCSS } from "./helpers";
+
+const cssWithoutAnchorPositioning = ".a { color: red; } .b { color: green; }";
+
+describe("parse stylesheet", () => {
+  it("parses and finds @position-fallback at-rule", () => {
+    const result = parsePositionFallback(sampleAnchorCSS);
+    expect(result).toBeTruthy();
+  });
+
+  it("parses and finds anchor() function", () => {
+    const result = parseAnchorFunctions(sampleAnchorCSS);
+    expect(result).toBeTruthy();
+  });
+
+  it("parses and finds @position-fallback at-rule and anchor() function", () => {
+    const result = parseCSS(sampleAnchorCSS);
+    expect(result).toBeTruthy();
+  });
+
+  it("does not find @position-fallback at-rule or anchor() function", () => {
+    const result = parseCSS(cssWithoutAnchorPositioning);
+    expect(result).toBeFalsy();
+  });
+
+  it("can collect @position-fallback values to transform", () => {
+    const result = parseCSS(sampleAnchorCSS);
+    expect(result).toBe("123");
+  });
+
+  it("can collect anchor() values to transform", () => {
+    const result = parseCSS(sampleAnchorCSS);
+    expect(result).toBe("123");
+  });
+
+  it("can return all necessary anchor() and @position-fallback values to transform", () => {
+    const result = parseCSS(sampleAnchorCSS);
+    expect(result).toBe("123");
+  });
+});
