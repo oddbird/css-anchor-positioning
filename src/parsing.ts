0bea40726d22e29bb4aa919b37dfb4bffe15dc14
Jonny Gerig Meyer <jonny@oddbird.net>
2022-08-04 18:22:13 -0400

    lint + test

--- src/parsing.ts
+++ src/parsing.ts
@@ -17,21 +17,23 @@ export function parsePositionFallback(ast: csstree.CssNode) {
 
   if (parsedFallbackRules) {
     parsedFallbackRules.forEach((fallback: csstree.Atrule) => {
-      const fallbackName = fallback.prelude.value;
+      const fallbackName = (fallback.prelude as csstree.Raw).value;
       const fallbackTryBlocks: TryBlockMap[] = [];
 
-      fallback.block.children?.forEach((childBlock) => {
+      fallback.block.children?.forEach((childBlock: csstree.Atrule) => {
         const tryBlock: TryBlockMap = {};
 
         // First edge position in try block is the `head` in the AST
-        //  data.property : the edge direction (i.e. top, left, bottom, right)
-        //  data.value.value : the anchor function (i.e. anchor(--button left))
-        const firstEdge = childBlock.block.children.head;
-        tryBlock[firstEdge.data.property] = firstEdge.data.value.value;
+        //  property : the edge direction (i.e. top, left, bottom, right)
+        //  value.value : the anchor function (i.e. anchor(--button left))
+        const firstEdge = childBlock.block.children
+          .first as unknown as csstree.Declaration;
+        tryBlock[firstEdge.property] = (firstEdge.value as csstree.Raw).value;
 
         // Second edge position in try block is the `tail` in the AST
-        const secondEdge = childBlock.block.children.tail;
-        tryBlock[secondEdge.data.property] = secondEdge.data.value.value;
+        const secondEdge = childBlock.block.children
+          .last as unknown as csstree.Declaration;
+        tryBlock[secondEdge.property] = (secondEdge.value as csstree.Raw).value;
 
         fallbackTryBlocks.push(tryBlock);
       });
--- tests/parsing.test.ts
+++ tests/parsing.test.ts
@@ -1,14 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { test, expect } from '@playwright/test';
 
 import { parseCSS } from '../src/parsing.js';
-import { sampleAnchorCSS } from './helpers.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sampleStyles = fs.readFileSync(path.join(__dirname, 'specExample.css'), {
+  encoding: 'utf8',
+});
 
 const cssWithoutAnchorPositioning = '.a { color: red; } .b { color: green; }';
 
 test.describe('parse stylesheet', () => {
   test('parses and returns @position-fallback strategy', () => {
-    const result = parseCSS(sampleAnchorCSS);
-    expect(result).toBeTruthy();
+    const result = parseCSS(sampleStyles);
+
     expect(result).toEqual({
       '--button-popup': [
         { top: 'anchor(--button bottom)', left: 'anchor(--button left)' },
@@ -21,6 +30,7 @@ test.describe('parse stylesheet', () => {
 
   test('does not find @position-fallback at-rule', () => {
     const result = parseCSS(cssWithoutAnchorPositioning);
+
     expect(result.fallbackStrategy).toBeUndefined();
   });
 });
