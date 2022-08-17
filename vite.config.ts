2c5986804c532e3423e85142345f87d383510b36
Jonny Gerig Meyer <jonny@oddbird.net>
2022-08-17 11:03:36 -0400

    jest -> vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
@@ -14,5 +16,29 @@ export default defineConfig({
     },
     target: 'es6',
     sourcemap: true,
+  },
+  /**
+   * @see https://vitest.dev/config/#configuration
+   */
+  test: {
+    include: ['./tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    globals: true,
+    environment: 'jsdom',
+    watch: false,
+    clearMocks: true,
+    reporters: 'dot',
+    coverage: {
+      enabled: true,
+      reporter: ['text-summary', 'html'],
+      include: ['src/**/*.{js,ts}'],
+      exclude: ['src/index.ts'],
+      all: true,
+      skipFull: true,
+      // Threshold
+      statements: 90,
+      branches: 90,
+      functions: 65,
+      lines: 90,
+    },
   },
 });
