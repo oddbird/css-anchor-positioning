47eacc67e4d545a711ec52da6cd433fa0abcab21
Jonny Gerig Meyer <jonny@oddbird.net>
2022-09-07 16:40:59 -0400

    Add Netlify deploy commands

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,18 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'CssAnchorPositioning',
-      // the proper extensions will be added
-      fileName: 'css-anchor-positioning',
-    },
-    target: 'es6',
-    sourcemap: true,
-  },
+  build: process.env.NETLIFY
+    ? {}
+    : {
+        lib: {
+          entry: resolve(__dirname, 'src/index.ts'),
+          name: 'CssAnchorPositioning',
+          // the proper extensions will be added
+          fileName: 'css-anchor-positioning',
+        },
+        target: 'es6',
+        sourcemap: true,
+      },
   /**
    * @see https://vitest.dev/config/#configuration
    */
