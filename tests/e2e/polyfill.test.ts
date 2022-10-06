c7ae6331036f289f958ea5a3c0716e6102931057
Jonny Gerig Meyer <jonny@oddbird.net>
2022-10-06 12:12:55 -0400

    tests

--- tests/e2e/polyfill.test.ts
+++ tests/e2e/polyfill.test.ts
@@ -1,6 +1,8 @@
 import { type Page, expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
+  // Listen for all console logs
+  page.on('console', (msg) => console.log(msg.text()));
   await page.goto('/');
 });
 
@@ -13,39 +15,55 @@ async function applyPolyfill(page: Page) {
   return await btn.click();
 }
 
+async function getAnchorWidth(page: Page) {
+  return page
+    .locator(anchorSelector)
+    .evaluate((node: HTMLElement) => node.getBoundingClientRect().width);
+}
+
 async function getParentWidth(page: Page) {
   return page
     .locator(floatingSelector)
     .evaluate((node: HTMLElement) => node.offsetParent?.clientWidth ?? 0);
 }
 
+async function getParentHeight(page: Page) {
+  return page
+    .locator(floatingSelector)
+    .evaluate((node: HTMLElement) => node.offsetParent?.clientHeight ?? 0);
+}
+
 test('applies polyfill', async ({ page }) => {
   const floating = page.locator(floatingSelector);
+  const width = await getAnchorWidth(page);
   const parentWidth = await getParentWidth(page);
-  const expected = `${parentWidth - 200}px`;
+  const parentHeight = await getParentHeight(page);
+  const expected = `${parentWidth - width}px`;
 
   await expect(floating).toHaveCSS('top', '0px');
   await expect(floating).not.toHaveCSS('right', expected);
 
   await applyPolyfill(page);
 
-  await expect(floating).toHaveCSS('top', '100px');
+  await expect(floating).toHaveCSS('top', `${parentHeight}px`);
   await expect(floating).toHaveCSS('right', expected);
 });
 
 test('updates when sizes change', async ({ page }) => {
-  const parentWidth = await getParentWidth(page);
   const floating = page.locator(floatingSelector);
+  const width = await getAnchorWidth(page);
+  const parentWidth = await getParentWidth(page);
+  const parentHeight = await getParentHeight(page);
   await applyPolyfill(page);
-  let expected = `${parentWidth - 200}px`;
+  let expected = `${parentWidth - width}px`;
 
-  await expect(floating).toHaveCSS('top', '100px');
+  await expect(floating).toHaveCSS('top', `${parentHeight}px`);
   await expect(floating).toHaveCSS('right', expected);
 
   await page
     .locator(anchorSelector)
     .evaluate((anchor) => (anchor.style.width = '50px'));
-  expected = `${parentWidth - 150}px`;
+  expected = `${parentWidth - 50}px`;
 
   await expect(floating).toHaveCSS('right', expected);
 });
