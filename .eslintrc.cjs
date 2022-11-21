f7f0a8175ab58979a97d1394840ccedb6910965f
Jonny Gerig Meyer <jonny@oddbird.net>
2022-11-21 12:44:03 -0500

    do not warn on TS version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
+    warnOnUnsupportedTypeScriptVersion: false,
   },
   plugins: ['@typescript-eslint', 'simple-import-sort'],
   settings: {
