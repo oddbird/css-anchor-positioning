558d9cbf44ae21084643b6381a90e9b2a48e08a4
Ed Rivas <ed@oddbird.net>
2022-11-23 02:06:45 +0000

    Switch to liquid templates

--- tests/report.ts
+++ tests/report.ts
@@ -1,31 +1,18 @@
 import fs from 'fs';
-
+import { Liquid } from 'liquidjs';
 import type { BrowserDefinition, ResultData } from './wpt.js';
 
 export interface VersionResult {
   name: string;
   summary: [number, number];
 }
-export interface TestPathMap {
-  [key: string]: VersionResult[];
-}
 
 const localDomain = 'http://web-platform.test:8000/';
-
-function gradientBar(active: number, total: number): string {
-  const stop = `${((active / total) * 100).toFixed(0)}%`;
-  return `style="background-image: linear-gradient(to right, darkgreen ${stop}, maroon ${stop})"`;
-}
 
 export default function writeReport(
   results: BrowserDefinition[],
   name?: string,
 ) {
-  const wptRepo: string | undefined = process.env.WPT_REPO;
-  const wptCommit: string | undefined = process.env.WPT_COMMIT;
-  const sourceRepo: string | undefined = process.env.SOURCE_REPO;
-  const sourceCommit: string | undefined = process.env.SOURCE_COMMIT;
-
   const timeStamp = new Date().toISOString();
   const fileName = name || timeStamp.replaceAll(':', '-');
   if (!fs.existsSync('test-results')) fs.mkdirSync('test-results');
@@ -34,7 +21,7 @@ export default function writeReport(
   fs.writeFileSync(`test-results/${fileName}.json`, JSON.stringify(results));
 
   // Create an object mapping each test path with the results for all versions
-  const byPath: TestPathMap = {};
+  const resultsByPath: Record<string, VersionResult[]> = {};
   results.forEach((browser) => {
     browser.versions.forEach((version) => {
       const data = version.data as ResultData;
@@ -49,97 +36,26 @@ export default function writeReport(
           name: `${browser.name} ${version.name}`,
           summary: total === undefined ? [-1, -1] : [passed, total],
         };
-        byPath[path] ? byPath[path].push(data) : (byPath[path] = [data]);
+        resultsByPath[path] = [...(resultsByPath[path] || []), data];
       });
     });
   });
 
   // Render the HTML report
-  // Each test gets a row, and the columns are the browser versions
-  const tableHtml = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>WPT Report</title>
-    <style>
-      td {border: 1px solid}
-      .test-name {display: flex; justify-content: space-between; gap: 1em}
-      .test-bar {color: white; background-color: maroon}
-    </style>
-  </head>
-  <body>
-  <p>Generated at: ${timeStamp}</p>
-  ${
-    sourceRepo && sourceCommit
-      ? `<p>
-          Source commit:
-          <a target="_blank" href="https://github.com/${sourceRepo}/commit/${sourceCommit}">
-            ${sourceCommit}
-          </a>
-        </p>`
-      : ''
-  }
-  ${
-    wptRepo && wptCommit
-      ? `<p>
-          WPT commit:
-          <a target="_blank" href="https://github.com/${wptRepo}/commit/${wptCommit}">
-            ${wptCommit}
-          </a>
-        </p>`
-      : ''
-  }
-  <br><a href="history.html">History</a>
-  <table>
-    <thead>
-      <tr>
-        <th>Test</th>
-        ${results
-          .map((browser) =>
-            browser.versions
-              .map((version) => `<th>${browser.name}<br>${version.name}</th>`)
-              .join(''),
-          )
-          .join('')}
-      </tr>
-    </thead>
-    <tbody>
-        ${Object.keys(byPath)
-          .map(
-            (testPath) =>
-              `<tr>
-                <td class="test-name">
-                  ${
-                    wptRepo && wptCommit
-                      ? `<a target="_blank" href="https://github.com/${wptRepo}/blob/${wptCommit}/${testPath}">
-                          ${testPath}
-                        </a>`
-                      : testPath
-                  }
-                  <span>
-                    <a target="_blank" href="https://wpt.live/${testPath}" title="Open in wpt.live">🌐</a>
-                    <a target="_blank" href="${localDomain}${testPath}" title="Open locally">🏠</a>
-                  </span>
-                </td>
-                ${byPath[testPath]
-                  .map(
-                    ({ summary: [pass, total] }) =>
-                      `<td class="test-bar" ${gradientBar(pass, total)}>
-                        ${pass} / ${total}
-                      </td>`,
-                  )
-                  .join('')}
-              </tr>`,
-          )
-          .join('')}
-    </tbody>
-  </table>
-  </body>
-  </html>
-  `;
-
+  const template = fs.readFileSync('tests/report.liquid', 'utf-8');
+  const context = {
+    wptRepo: process.env.WPT_REPO,
+    wptCommit: process.env.WPT_COMMIT,
+    sourceRepo: process.env.SOURCE_REPO,
+    sourceCommit: process.env.SOURCE_COMMIT,
+    featureName: process.env.FEATURE_NAME || process.env.SOURCE_REPO,
+    timeStamp,
+    localDomain,
+    results,
+    resultsByPath,
+  };
+  const output = new Liquid().parseAndRenderSync(template, context);
   // Save with timestamp and as `index.html` to load the latest report by default
-  fs.writeFileSync(`test-results/${fileName}.html`, tableHtml);
-  fs.writeFileSync(`test-results/index.html`, tableHtml);
+  fs.writeFileSync(`test-results/${fileName}.html`, output);
+  fs.writeFileSync(`test-results/index.html`, output);
 }
