feb6675d898980a1f0fcc1f4245f5684ba455915
Derek Gray <tuespetre@users.noreply.github.com>
2024-09-12 18:52:30 -0500

    Fix #242: keep id,media,title attributes on replaced link elements

--- tests/unit/transform.test.ts
+++ tests/unit/transform.test.ts
@@ -49,4 +49,24 @@ describe('transformCSS', () => {
     expect(div.hasAttribute('data-has-inline-styles')).toBeFalsy();
     expect(div2.hasAttribute('data-has-inline-styles')).toBeFalsy();
   });
+
+  it('preserves id, media, and title attributes when replacing link elements', async () => {
+    document.head.innerHTML = `
+      <link id="the-link" media="screen" title="stylish" rel="stylesheet" href="/sample.css"/>
+    `;
+    let link = document.querySelector('link') as HTMLLinkElement;
+    const styleData = [
+      { el: link, css: 'html { margin: 0; }', changed: true },
+    ];
+    const inlineStyles = new Map();
+    const promise = transformCSS(styleData, inlineStyles, true);
+    link = document.querySelector('link') as HTMLLinkElement;
+    link.dispatchEvent(new Event('load'));
+    await promise;
+
+    expect(link.href).toContain('/updated.css');
+    expect(link.id).toBe('the-link');
+    expect(link.media).toBe('screen');
+    expect(link.title).toBe('stylish');
+  });
 });
