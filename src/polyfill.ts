4eee237233e0864e509ddfad786012685b6e5930
Jonny Gerig Meyer <jonny@oddbird.net>
2024-10-16 10:30:08 -0400

    Export new AnchorPositioningPolyfillOptions type

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -443,6 +443,23 @@ async function position(rules: AnchorPositions, useAnimationFrame = false) {
   }
 }
 
+export interface AnchorPositioningPolyfillOptions {
+  // Whether to use `requestAnimationFrame()` when updating target elements’
+  // positions
+  useAnimationFrame?: boolean;
+
+  // An array of explicitly targeted elements to polyfill
+  elements?: HTMLElement[];
+
+  // Whether to exclude elements with eligible inline styles. When not defined
+  // or set to `false`, the polyfill will be applied to all elements that have
+  // eligible inline styles, regardless of whether the `elements` option is
+  // defined. When set to `true`, elements with eligible inline styles listed
+  // in the `elements` option will still be polyfilled, but no other elements
+  // in the document will be implicitly polyfilled.
+  excludeInlineStyles?: boolean;
+}
+
 function normalizePolyfillOptions(
   useAnimationFrameOrOption: boolean | AnchorPositioningPolyfillOptions = {},
 ) {
