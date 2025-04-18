feb0397d046f88616dc67d50c68cebcd4c60eb6a
James Stuckey Weber <james@oddbird.net>
2025-04-18 12:30:57 -0400

    Use style elements in head rather than blob links

--- src/parse.ts
+++ src/parse.ts
@@ -684,28 +684,30 @@ export async function parseCSS(styleData: StyleData[]) {
     }
   }
 
-  for (const styleObj of styleData) {
-    if (styleObj.changed) {
-      let changed = false;
-      const ast = getAST(styleObj.css);
-      walk(ast, {
-        visit: 'Atrule',
-        enter(node) {
-          changed = makeImportUrlAbsolute(node);
-        },
-      });
-      walk(ast, {
-        visit: 'Declaration',
-        enter(node) {
-          changed = makeDeclarationValueUrlAbsolute(node) || changed;
-        },
-      });
-      if (changed) {
-        // Update CSS
-        styleObj.css = generateCSS(ast);
-      }
-    }
-  }
+  // for (const styleObj of styleData) {
+  //   if (styleObj.changed) {
+  //     let changed = false;
+  //     const ast = getAST(styleObj.css);
+  //     // @import rules need to be reparsed, as atrule preludes are not parsed
+  //     // by default
+  //     walk(ast, {
+  //       visit: 'Atrule',
+  //       enter(node) {
+  //         changed = makeImportUrlAbsolute(node);
+  //       },
+  //     });
+  //     walk(ast, {
+  //       visit: 'Declaration',
+  //       enter(node) {
+  //         changed = makeDeclarationValueUrlAbsolute(node) || changed;
+  //       },
+  //     });
+  //     if (changed) {
+  //       // Update CSS
+  //       styleObj.css = generateCSS(ast);
+  //     }
+  //   }
+  // }
 
   // Store inline style custom property mappings for each target element
   const inlineStyles = new Map<HTMLElement, Record<string, string>>();
