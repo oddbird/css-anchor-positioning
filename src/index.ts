5159724307c31770fb3e6b4ae0b7a6832615c0a8
Burton Smith <31320098+break-stuff@users.noreply.github.com>
2026-02-11 09:55:38 -0500

    Skip Polyfill for SSR and non-browser environments (#377)
    * added skip for ssr and non-browser environments

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { polyfill } from './polyfill.js';
 
 // apply polyfill
-if (document.readyState !== 'complete') {
-  window.addEventListener('load', () => {
+if (typeof window !== 'undefined') {
+  if (document.readyState !== 'complete') {
+    window.addEventListener('load', () => {
+      polyfill();
+    });
+  } else {
     polyfill();
-  });
-} else {
-  polyfill();
+  }
 }
